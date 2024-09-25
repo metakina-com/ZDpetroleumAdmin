229f5a4429ab48ed85ea63c530ea530e6c39b777
mkderKH <58474570+mkderKH@users.noreply.github.com>
2024-09-25 14:08:06 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -32,14 +32,14 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
   {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
   },
   // {
   //   path: '/',
@@ -53,79 +53,83 @@ export const constantRoutes = [
   //   }]
   // },
   {
-    path: '/group',
+    path: "/group",
     component: Layout,
 
-         children: [{
-      path: 'group',
-      name: 'Group',
-      component: () => import('@/views/group/index'),
-      meta: { title: '团队数据', icon: 'dashboard' }
-    }],
-    hidden: true
+    children: [
+      {
+        path: "group",
+        name: "Group",
+        component: () => import("@/views/group/index"),
+        meta: { title: "团队数据", icon: "dashboard" },
+      },
+    ],
+    hidden: true,
   },
   {
-    path: '/user',
+    path: "/user",
     component: Layout,
-    redirect: '/userlist',
-    meta: { title: '用户管理', icon: 'el-icon-s-custom' },
-    children: [{
-      path: '/userlist',
-      name: 'Userlist',
-      component: () => import('@/views/user/user/index'),
-      meta: { title: '用户管理', icon: 'user' }
-    },
-    // {
-    //   path: '/partner',
-    //   name: 'Partner',
-    //   component: () => import('@/views/user/partner/index'),
-    //   meta: { title: '合伙人管理', icon: 'el-icon-s-check' }
-    // }
-    // {
-    //   path: '/userassets',
-    //   name: 'Userassets',
-    //   component: () => import('@/views/user/user_assets/index'),
-    //   meta: { title: '用户资产', icon: 'el-icon-coin' }
-    // },
-    // {
-    //   path: '/usertree',
-    //   name: 'Usertree',
-    //   component: () => import('@/views/user/user_tree/index'),
-    //   meta: { title: '用户层级关系', icon: 'el-icon-s-unfold' }
-    // },
-    // {
-    //   path: '/tree',
-    //   name: 'Tree',
-    //   component: () => import('@/views/user/tree/index'),
-    //   meta: { title: '用户层级树', icon: 'el-icon-s-operation' }
-    // },
-    // {
-    //   path: '/realname',
-    //   name: 'realname',
-    //   component: () => import('@/views/user/realname/index'),
-    //   meta: { title: '实名审核', icon: 'el-icon-s-check' }
-    // },
-    ]
+    redirect: "/userlist",
+    meta: { title: "用户管理", icon: "el-icon-s-custom" },
+    children: [
+      {
+        path: "/userlist",
+        name: "Userlist",
+        component: () => import("@/views/user/user/index"),
+        meta: { title: "用户管理", icon: "user" },
+      },
+      // {
+      //   path: '/partner',
+      //   name: 'Partner',
+      //   component: () => import('@/views/user/partner/index'),
+      //   meta: { title: '合伙人管理', icon: 'el-icon-s-check' }
+      // }
+      // {
+      //   path: '/userassets',
+      //   name: 'Userassets',
+      //   component: () => import('@/views/user/user_assets/index'),
+      //   meta: { title: '用户资产', icon: 'el-icon-coin' }
+      // },
+      // {
+      //   path: '/usertree',
+      //   name: 'Usertree',
+      //   component: () => import('@/views/user/user_tree/index'),
+      //   meta: { title: '用户层级关系', icon: 'el-icon-s-unfold' }
+      // },
+      // {
+      //   path: '/tree',
+      //   name: 'Tree',
+      //   component: () => import('@/views/user/tree/index'),
+      //   meta: { title: '用户层级树', icon: 'el-icon-s-operation' }
+      // },
+      // {
+      //   path: '/realname',
+      //   name: 'realname',
+      //   component: () => import('@/views/user/realname/index'),
+      //   meta: { title: '实名审核', icon: 'el-icon-s-check' }
+      // },
+    ],
   },
 
   {
-    path: '/wallet',
+    path: "/wallet",
     component: Layout,
-    redirect: '/wallet',
-    meta: { title: '钱包管理', icon: 'el-icon-s-finance' },
-    children: [{
-      path: '/wallet_list',
-      name: 'Wallet',
-      component: () => import('@/views/wallet/wallet_list/index'),
-      meta: { title: '钱包管理', icon: 'el-icon-s-finance' }
-    },
-    {
-      path: '/user_address',
-      name: 'userAddress',
-      component: () => import('@/views/wallet/user_address/index'),
-      meta: { title: '用户地址', icon: 'el-icon-wallet' }
-    },
-    ]
+    redirect: "/wallet",
+    meta: { title: "钱包管理", icon: "el-icon-s-finance" },
+    children: [
+      {
+        path: "/wallet_list",
+        name: "Wallet",
+        component: () => import("@/views/wallet/wallet_list/index"),
+        meta: { title: "钱包管理", icon: "el-icon-s-finance" },
+      },
+      {
+        path: "/user_address",
+        name: "userAddress",
+        component: () => import("@/views/wallet/user_address/index"),
+        meta: { title: "用户地址", icon: "el-icon-wallet" },
+      },
+    ],
   },
 
   // {
@@ -153,7 +157,7 @@ export const constantRoutes = [
   //   },
   //   ]
   // },
-  
+
   // {
   //   path: '/contract',
   //   component: Layout,
@@ -162,7 +166,7 @@ export const constantRoutes = [
   //     title: '合约系统', icon: 'el-icon-folder-opened'
   //   },
   //   children: [
-      
+
   //     {
   //       path: '/chicang',
   //       name: 'Chicang',
@@ -213,19 +217,20 @@ export const constantRoutes = [
   //     },]
   // },
   {
-    path: '/fnancialmanagement',
+    path: "/fnancialmanagement",
     component: Layout,
-    redirect: '/fnancialmanagement',
-    meta: { title: '理财管理', icon: 'el-icon-s-ticket' },
-    children: [{
-      path: 'broker',
-      name: 'Broker',
-      component: () => import('@/views/fnancialmanagement/index'),
-      meta: { title: '理财设置', icon: 'el-icon-money' }
-    },
-    ]
+    redirect: "/fnancialmanagement",
+    meta: { title: "理财管理", icon: "el-icon-s-ticket" },
+    children: [
+      {
+        path: "broker",
+        name: "Broker",
+        component: () => import("@/views/fnancialmanagement/index"),
+        meta: { title: "理财设置", icon: "el-icon-money" },
+      },
+    ],
   },
- 
+
   // {
   //   path: '/follow',
   //   component: Layout,
@@ -295,108 +300,117 @@ export const constantRoutes = [
   //   }]
   // },
   {
-    path: '/legalcurrency',
+    path: "/legalcurrency",
     component: Layout,
-    redirect: '/legalcurrency',
-    children: [{
-      path: 'legalcurrency',
-      name: 'legalcurrency',
-      component: () => import('@/views/legalcurrency/index'),
-      meta: { title: '法币交易', icon: 'el-icon-s-check' }
-    }]
+    redirect: "/legalcurrency",
+    children: [
+      {
+        path: "legalcurrency",
+        name: "legalcurrency",
+        component: () => import("@/views/legalcurrency/index"),
+        meta: { title: "法币交易", icon: "el-icon-s-check" },
+      },
+    ],
   },
   /* new */
   {
-    path: '/follow',
+    path: "/follow",
     component: Layout,
-    redirect: '/follow',
+    redirect: "/follow",
     meta: {
-      title: '合约跟单系统', icon: 'el-icon-folder'
+      title: "合约跟单系统",
+      icon: "el-icon-folder",
     },
     children: [
       {
-        path: '/follow_apply',
-        name: 'followApply',
-        component: () => import('@/views/follow/apply/index'),
-        meta: { title: '交易员申请', icon: 'el-icon-user' }
+        path: "/follow_apply",
+        name: "followApply",
+        component: () => import("@/views/follow/apply/index"),
+        meta: { title: "交易员申请", icon: "el-icon-user" },
       },
       {
-        path: '/follow_chicang',
-        name: 'followChicang',
-        component: () => import('@/views/follow/chicang/index'),
-        meta: { title: '跟单持仓', icon: 'el-icon-document-checked' }
-      }, {
-        path: '/follow_entrust',
-        name: 'followEntrust',
-        component: () => import('@/views/follow/entrust/index'),
-        meta: { title: '跟单委托', icon: 'el-icon-tickets' }
-      }, {
-        path: '/follow_history_entrust',
-        name: 'followHistoryEntrust',
-        component: () => import('@/views/follow/historyEntrust/index'),
-        meta: { title: '历史委托', icon: 'el-icon-document' }
-      }, {
-        path: '/follow_history',
-        name: 'followHistory',
-        component: () => import('@/views/follow/history/index'),
-        meta: { title: '历史记录', icon: 'el-icon-document' }
-      }, {
-        path: '/income',
-        name: 'income',
-        component: () => import('@/views/follow/income/index'),
-        meta: { title: '交易员收益', icon: 'el-icon-document' }
-      }]
+        path: "/follow_chicang",
+        name: "followChicang",
+        component: () => import("@/views/follow/chicang/index"),
+        meta: { title: "跟单持仓", icon: "el-icon-document-checked" },
+      },
+      {
+        path: "/follow_entrust",
+        name: "followEntrust",
+        component: () => import("@/views/follow/entrust/index"),
+        meta: { title: "跟单委托", icon: "el-icon-tickets" },
+      },
+      {
+        path: "/follow_history_entrust",
+        name: "followHistoryEntrust",
+        component: () => import("@/views/follow/historyEntrust/index"),
+        meta: { title: "历史委托", icon: "el-icon-document" },
+      },
+      {
+        path: "/follow_history",
+        name: "followHistory",
+        component: () => import("@/views/follow/history/index"),
+        meta: { title: "历史记录", icon: "el-icon-document" },
+      },
+      {
+        path: "/income",
+        name: "income",
+        component: () => import("@/views/follow/income/index"),
+        meta: { title: "交易员收益", icon: "el-icon-document" },
+      },
+    ],
   },
   {
-    path: '/qihuo',
+    path: "/qihuo",
     component: Layout,
-    redirect: '/chicang',
+    redirect: "/chicang",
     meta: {
-      title: '期权系统', icon: 'el-icon-folder-opened'
+      title: "期权系统",
+      icon: "el-icon-folder-opened",
     },
     children: [
+      {
+        path: "/QHchicang",
+        name: "QHchicang",
+        component: () => import("@/views/qihuo/chicang/index"),
+        meta: { title: "交割订单", icon: "el-icon-document-checked" },
+      },
 
       {
-        path: '/QHchicang',
-        name: 'QHchicang',
-        component: () => import('@/views/qihuo/chicang/index'),
-        meta: { title: '交割订单', icon: 'el-icon-document-checked' }
+        path: "/QHhistory",
+        name: "QHHistory",
+        component: () => import("@/views/qihuo/history/index"),
+        meta: { title: "交割历史", icon: "el-icon-document" },
       },
-     
-      {
-        path: '/QHhistory',
-        name: 'QHHistory',
-        component: () => import('@/views/qihuo/history/index'),
-        meta: { title: '交割历史', icon: 'el-icon-document' }
-      },
-    //  {
-    //     path: '/QHuser',
-    //     name: 'QHuser',
-    //     component: () => import('@/views/qihuo/QHuser/index'),
-    //     meta: { title: '用户列表', icon: 'el-icon-document' }
-    //   },
-    ]
+      //  {
+      //     path: '/QHuser',
+      //     name: 'QHuser',
+      //     component: () => import('@/views/qihuo/QHuser/index'),
+      //     meta: { title: '用户列表', icon: 'el-icon-document' }
+      //   },
+    ],
   },
   /*  */
   {
-    path: '/bibi',
+    path: "/bibi",
     component: Layout,
-    redirect: '/bibi',
+    redirect: "/bibi",
     meta: {
-      title: '币币系统', icon: 'el-icon-c-scale-to-original'
+      title: "币币系统",
+      icon: "el-icon-c-scale-to-original",
     },
     children: [
       {
-        path: '/bibiEntrust',
-        name: 'BibiEntrust',
-        component: () => import('@/views/bibi/entrust/index'),
-        meta: { title: '当前委托', icon: 'el-icon-tickets' }
+        path: "/bibiEntrust",
+        name: "BibiEntrust",
+        component: () => import("@/views/bibi/entrust/index"),
+        meta: { title: "当前委托", icon: "el-icon-tickets" },
       },
       {
-        path: '/bibiHistory',
-        name: 'BibiHistory',
-        component: () => import('@/views/bibi/history/index'),
-        meta: { title: '历史委托', icon: 'el-icon-document' }
+        path: "/bibiHistory",
+        name: "BibiHistory",
+        component: () => import("@/views/bibi/history/index"),
+        meta: { title: "历史委托", icon: "el-icon-document" },
       },
       // {
       //   path: '/Historys',
@@ -404,19 +418,19 @@ export const constantRoutes = [
       //   component: () => import('@/views/bibi/historys/index'),
       //   meta: { title: '历史记录', icon: 'el-icon-document' }
       // },
-      ]
+    ],
   },
   {
-    path: '/assets',
+    path: "/assets",
     component: Layout,
-    redirect: '/recharge',
-    meta: { title: '出入金统计', icon: 'el-icon-s-order' },
+    redirect: "/recharge",
+    meta: { title: "出入金统计", icon: "el-icon-s-order" },
     children: [
       {
-        path: '/check',
-        name: 'Check',
-        component: () => import('@/views/assets/withdrawal_check/index'),
-        meta: { title: '提币审核', icon: 'el-icon-upload2' }
+        path: "/check",
+        name: "Check",
+        component: () => import("@/views/assets/withdrawal_check/index"),
+        meta: { title: "提币审核", icon: "el-icon-upload2" },
       },
       // {
       //   path: '/recharge',
@@ -425,45 +439,45 @@ export const constantRoutes = [
       //   meta: { title: '充币审核', icon: 'el-icon-refresh' }
       // },
       {
-        path: '/withdrawal',
-        name: 'Withdrawal',
-        component: () => import('@/views/assets/withdrawal/index'),
-        meta: { title: '提币明细', icon: 'el-icon-printer' }
+        path: "/withdrawal",
+        name: "Withdrawal",
+        component: () => import("@/views/assets/withdrawal/index"),
+        meta: { title: "提币明细", icon: "el-icon-printer" },
       },
       {
-        path: '/charging',
-        name: 'charging',
-        component: () => import('@/views/assets/withdrawal/charging'),
-        meta: { title: '充币明细', icon: 'el-icon-printer' }
+        path: "/charging",
+        name: "charging",
+        component: () => import("@/views/assets/withdrawal/charging"),
+        meta: { title: "充币明细", icon: "el-icon-printer" },
       },
       {
-        path: '/transfer',
-        name: 'Transfer',
-        component: () => import('@/views/assets/transfer/index'),
-        meta: { title: '划转记录', icon: 'el-icon-aim' }
-      }]
+        path: "/transfer",
+        name: "Transfer",
+        component: () => import("@/views/assets/transfer/index"),
+        meta: { title: "划转记录", icon: "el-icon-aim" },
+      },
+    ],
   },
   {
-    path: '/risk',
+    path: "/risk",
     component: Layout,
-    redirect: '/risk',
-    meta: { title: '风控管理', icon: 'el-icon-s-order' },
+    redirect: "/risk",
+    meta: { title: "风控管理", icon: "el-icon-s-order" },
     children: [
       {
-        path: '/risk_con',
-        name: 'Risk',
-        component: () => import('@/views/risk_control/risk/index'),
-        meta: { title: '风控管理', icon: 'el-icon-refresh' }
+        path: "/risk_con",
+        name: "Risk",
+        component: () => import("@/views/risk_control/risk/index"),
+        meta: { title: "风控管理", icon: "el-icon-refresh" },
       },
       {
-        path: '/risk_ping',
-        name: 'Risk_ping',
-        component: () => import('@/views/risk_control/risk_feng/index'),
-        meta: { title: '开平仓时间', icon: 'el-icon-aim' }
+        path: "/risk_ping",
+        name: "Risk_ping",
+        component: () => import("@/views/risk_control/risk_feng/index"),
+        meta: { title: "开平仓时间", icon: "el-icon-aim" },
       },
-    ]
+    ],
   },
-
 
   // {
   //   path: '/example',
@@ -510,63 +524,62 @@ export const constantRoutes = [
   //     }
   //   ]
   // },
-	{
-	    path: '/system',
-	    component: Layout,
-	    redirect: '/systemslist',
-	    meta: {
-	      title: '系统设置',
-	      icon: 'el-icon-setting'
-	    },
-	    children: [
-	      {
-	        path: '/systems',
-	        name: 'System',
-	        component: () => import('@/views/system/system/index'),
-	        meta: {
-	          title: '轮播图管理',
-	          icon: 'el-icon-picture-outline'
-	        }
-	      },
-	      {
-	        path: '/notice',
-	        name: 'Notice',
-	        component: () => import('@/views/system/notice/index'),
-	        meta: {
-	          title: '公告',
-	          icon: 'el-icon-document-checked'
-	        }
-	      },
-        {
-	        path: '/customerservice',
-	        name: 'Customerservice',
-	        component: () => import('@/views/system/customerservice/index'),
-	        meta: {
-	          title: '客服咨询',
-	          icon: 'el-icon-phone-outline'
-	        }
-	      }
-	
-	
-	    ]
-	  },
+  {
+    path: "/system",
+    component: Layout,
+    redirect: "/systemslist",
+    meta: {
+      title: "系统设置",
+      icon: "el-icon-setting",
+    },
+    children: [
+      {
+        path: "/systems",
+        name: "System",
+        component: () => import("@/views/system/system/index"),
+        meta: {
+          title: "轮播图管理",
+          icon: "el-icon-picture-outline",
+        },
+      },
+      {
+        path: "/notice",
+        name: "Notice",
+        component: () => import("@/views/system/notice/index"),
+        meta: {
+          title: "公告",
+          icon: "el-icon-document-checked",
+        },
+      },
+      {
+        path: "/customerservice",
+        name: "Customerservice",
+        component: () => import("@/views/system/customerservice/index"),
+        meta: {
+          title: "客服咨询",
+          icon: "el-icon-phone-outline",
+        },
+      },
+    ],
+  },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
+  { path: "*", redirect: "/404", hidden: true },
+];
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
